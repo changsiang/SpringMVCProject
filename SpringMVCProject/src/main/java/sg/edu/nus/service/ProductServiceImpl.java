85453b1c630f54ce98a2cf77d7086d36470c4124
hikari8807 <checkingmymail@gmail.com>
2017-12-06 18:24:32 +0800

    Added ProductServiceImpl

---
+++ SpringMVCProject/src/main/java/sg/edu/nus/service/ProductServiceImpl.java
@@ -0,0 +1,72 @@
+package sg.edu.nus.service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import javax.annotation.Resource;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.transaction.annotation.Transactional;
+
+import sg.edu.nus.model.Product;
+import sg.edu.nus.repository.ProductRepository;
+
+public class ProductServiceImpl implements ProductService {
+	
+	@Resource
+	ProductRepository productRepository;
+
+	@Override
+	@Transactional
+	public ArrayList<Product> findAllProducts() {
+		ArrayList<Product> products = (ArrayList<Product>)productRepository.findAll();
+		return products;
+	}
+
+	@Override
+	@Transactional
+	public Product findProduct(int productId) {
+		return productRepository.getOne(productId);
+	}
+
+	@Override
+	@Transactional
+	public Product createProduct(Product product) {
+		return productRepository.saveAndFlush(product);
+	}
+
+	@Override
+	@Transactional
+	public Product changeProduct(Product product) {
+		return productRepository.saveAndFlush(product);
+	}
+
+	@Override
+	@Transactional
+	public void removeProduct(Product product) {
+		productRepository.delete(product);
+	}
+
+	@Override
+	public ArrayList<Product> findProductByID(int productId) {
+		return productRepository.findProductByID(productId);
+	}
+
+	@Override
+	public ArrayList<Product> findProductByDesc(String description) {
+		return productRepository.findProductByDesc(description);
+	}
+
+	@Override
+	public ArrayList<Product> findProductByColor(String color) {
+		return productRepository.findProductByColor(color);
+	}
+
+	@Override
+	@Transactional
+	public ArrayList<Product> findProductByManufacturer(String manufacturer) {
+		return productRepository.findProductByManufacturer(manufacturer);
+	}
+	
+
+}
